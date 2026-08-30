f55b48fecb4c50fbd22a73343663aeca34f81005
mustapha-raji-github <mustapha.raji.work@gmail.com>
2026-08-30 19:58:42 -0400

    Update users.test.js

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,6 +2,8 @@ const test = require("node:test");
 const assert = require("node:assert");
 const request = require("supertest");
 
+//const request = require("supertest");
+
 const app = require("../server");
 
 test("GET /health returns ok", async () => {
